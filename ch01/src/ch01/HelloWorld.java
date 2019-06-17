709f0ea92d8e7719e494b19088ee6a0cf8b2e6ec
java <java@user-PC>
2019-06-17 21:53:10 +0900

    2019/06/17 ch01 등록

---
+++ ch01/src/ch01/HelloWorld.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class HelloWorld {
+/*
+ *  날짜 : 2019/06/03
+ *  이름 : 이홍석
+ *  내용 : 자바 시작하기 교재  p2~p25 
+ */
+	public static void main(String[] args) {
+		System.out.println("Hello World~");
+		System.out.println("Hello Java!");
+	}
+}
---
+++ ch01/src/p17/Hello.java
@@ -0,0 +1,11 @@
+package p17;
+
+public class Hello {
+/*
+ * 작성자 : 이홍석
+ * 작성일 : 2019.06.03
+ */
+	public static void main(String[] args) {
+		 System.out.println("Hello, welcome to the java would");
+	}
+}
